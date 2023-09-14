06bbaf27cce6f14fa35f93308e971c6e82c7ad13
vvbs <131943481+vvbs@users.noreply.github.com>
2023-09-14 15:02:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 // 俳句のパターン
 const haikuPatterns = [
-    ["青い空", "白い雲", "夏の朝"],
-    ["景色に映える", "遠くの街に", "静かな朝に"],
-    ["花が咲く", "雨が降る", "春の風"],
+    ["青い空", "白い雲", "夏の日"],
+    ["秋の風", "紅葉の葉", "静かな朝"],
+    ["花が咲く", "風に揺れる", "春の日"],
+    // さらに他のパターンを追加できます
 ];
 
-// ボタン
+// ボタン要素を取得
 const generateButton = document.getElementById("generateButton");
 
-// 俳句を表示する要素
-const haikuElement = document.getElementById("haiku");
+// 俳句の各行を表示する要素を取得
+const lineElements = document.querySelectorAll(".line");
 
 // ボタンがクリックされたときの処理
 generateButton.addEventListener("click", () => {
-    const pattern = getRandomPattern(haikuPatterns);
-    const haiku = pattern.join("\n");
-    haikuElement.textContent = haiku;
+    const haiku = generateHaiku(haikuPatterns);
+    for (let i = 0; i < haiku.length; i++) {
+        lineElements[i].textContent = haiku[i];
+    }
 });
+
+// ランダムな俳句を生成する関数
+function generateHaiku(patterns) {
+    const pattern = getRandomPattern(patterns);
+    return pattern;
+}
 
 // ランダムな俳句パターンを選ぶ関数
 function getRandomPattern(patterns) {
